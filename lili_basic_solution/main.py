388ebc1f2836d54f1ce12f061bebcbb6abf9ce17
lilibenedikta <55950892+lilibenedikta@users.noreply.github.com>
2022-02-16 21:57:15 +0000

    dump output

--- lili_basic_solution/main.py
+++ lili_basic_solution/main.py
@@ -24,3 +24,4 @@ if __name__ == "__main__":
                 min_dist = distance
                 closest = row[fkeys].to_dict()
         out.append(closest)
+    Path("output.json").write_text(json.dumps(out))
