3e3de09f0a0ec7d12e48705b956488d1e5ebd881
Hanga Dormán <dormanhanga@gmail.com>
2022-02-17 01:50:07 +0100

    remove own solution

--- hanga_basic_solution/preprocess.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-if __name__ == "__main__":
-
-    raw_data = pd.read_csv("data.csv")
-    filtered_data = raw_data.loc[lambda df: df["keycode"] == "p"].drop_duplicates()
-    filtered_data.to_parquet("filtered_data.parquet")
-    
