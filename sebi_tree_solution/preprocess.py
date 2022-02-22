30e2d293ed61571b81d7511dc94fb98fda0d4e81
papsebestyen <pap.sebestyen@gmail.com>
2022-02-22 17:55:37 +0100

    kd-tree solution

---
+++ sebi_tree_solution/preprocess.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.neighbors import KDTree
+import pickle
+
+data_subset = pd.read_csv("data.csv")[
+    lambda _df: _df["entity_id"] == 0
+].drop_duplicates(subset=["msec", "subject", "trial"])
+
+pos_cols = [f"{ax}_position" for ax in ["x", "y", "z"]]
+
+tree = KDTree(data_subset.loc[:, pos_cols].to_numpy(), metric="euclidean")
+
+data_subset.to_parquet("data_subset.parquet")
+
+with open("kdtree.pickle", "wb") as file:
+    pickle.dump(tree, file)
