4bf4ff87e4c2427b8440a68df0ff70cee29c2f70
papsebestyen <pap.sebestyen@gmail.com>
2022-02-23 15:42:05 +0100

    reseting index on filtered data

--- sebi_tree_solution/preprocess.py
+++ sebi_tree_solution/preprocess.py
@@ -2,9 +2,11 @@ import pandas as pd
 from sklearn.neighbors import KDTree
 import pickle
 
-data_subset = pd.read_csv("data.csv")[
-    lambda _df: _df["entity_id"] == 0
-].drop_duplicates(subset=["msec", "subject", "trial"])
+data_subset = (
+    pd.read_csv("data.csv")[lambda _df: _df["entity_id"] == 0]
+    .drop_duplicates(subset=["msec", "subject", "trial"])
+    .reset_index()
+)
 
 pos_cols = [f"{ax}_position" for ax in ["x", "y", "z"]]
 
