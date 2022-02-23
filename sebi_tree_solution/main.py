88c1df2f6386b7b5dfc8983eb8d1db3d2025d5ad
papsebestyen <pap.sebestyen@gmail.com>
2022-02-23 01:21:29 +0100

    modify k-neighbours

--- sebi_tree_solution/main.py
+++ sebi_tree_solution/main.py
@@ -4,18 +4,20 @@ import json
 import pickle
 from pathlib import Path
 
+from sklearn.cluster import k_means
+
 pos_cols = [f"{ax}_position" for ax in ["x", "y", "z"]]
 
 if __name__ == "__main__":
     data = pd.read_parquet("data_subset.parquet")
 
     input_locations = json.loads(Path("input.json").read_text())
-    input_locations = list(map(lambda d: np.array(list(d.values())), input_locations))
+    input_locations = np.array([*map(lambda x: list(x.values()), input_locations)])
 
     with open("kdtree.pickle", "rb") as file:
         tree = pickle.load(file)
 
-    dist, ind = tree.query(input_locations)
+    dist, ind = tree.query(input_locations, k = 1000)
 
     results = data.iloc[[x[0] for x in ind], :][["msec", "subject", "trial"]].to_dict(
         "records"
