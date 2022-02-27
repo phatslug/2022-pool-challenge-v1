7d788846f322db8f6fe692c5fe9b6d2a2bbf4497
lilibenedikta <55950892+lilibenedikta@users.noreply.github.com>
2022-02-27 19:36:47 +0000

    update_preprocess.py

--- lili_lessbasic/preprocess.py
+++ lili_lessbasic/preprocess.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 data = pd.read_csv("data.csv")
-data = data,loc[data["entity_id"] == 0].drop_duplicates().reset_index()
+data = data.loc[data["entity_id"] == 0].drop_duplicates().reset_index()
 data.to_pickle("data.pkl")
