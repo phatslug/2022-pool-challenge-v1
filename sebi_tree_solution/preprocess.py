1d8c5fa0637570207d227d23d57854ab1a5c5e00
papsebestyen <pap.sebestyen@gmail.com>
2022-02-28 23:58:11 +0100

    run with subprocess

--- sebi_tree_solution/preprocess.py
+++ sebi_tree_solution/preprocess.py
@@ -5,6 +5,7 @@ import uvicorn
 from pydantic import BaseModel
 from typing import List
 import numpy as np
+from subprocess import Popen
 
 app = FastAPI()
 
@@ -50,4 +51,5 @@ def get_index(input_json: ItemList):
 
 
 if __name__ == "__main__":
-    uvicorn.run("preprocess:app", host="127.0.0.1", port=5000)
+    # uvicorn.run("preprocess:app", host="127.0.0.1", port=5000)
+    Popen(['uvicorn', 'preprocess:app', '--host','127.0.0.1', '--port', '5000'])
