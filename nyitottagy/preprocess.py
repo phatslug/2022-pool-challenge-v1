0a2e9a99abf46355da4400b2765df7db369c2520
arturvelk <64686836+arturvelk@users.noreply.github.com>
2022-02-28 18:38:18 +0100

    kivettem a szaros második beolvasást, bocsi!

--- nyitottagy/preprocess.py
+++ nyitottagy/preprocess.py
@@ -6,14 +6,15 @@ data = pd.read_csv("data.csv")
 
 cols_1 = ['msec', 'x_position', 'y_position', 'z_position', 'subject', 'trial']
 cols = ["x_position","y_position","z_position"]
-data = pd.read_csv("data.csv")
+
 data = data.loc[data["entity_id"] == 0].drop_duplicates().reset_index(drop = True)
 data = data.loc[:,cols_1]
 
 for i in ["x_position", "y_position","z_position"]:
     data[i] = data[i].astype(np.float32)
-for i in ["msec", "trial"]:
-    data[i] = data[i].astype(np.int16)
+
+data["msec"] = data["msec"].astype(np.int32)
+data["trial"] = data["trial"].astype(np.int8)
 data["subject"] = data["subject"].astype("category")
 
 data.to_pickle("data.pkl")
