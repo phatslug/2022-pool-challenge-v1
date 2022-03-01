9326912f01e43d86d1c468eead773fafe4fe87ac
papsebestyen <pap.sebestyen@gmail.com>
2022-03-01 13:20:19 +0100

    sleep while server is not running

--- sebi_tree_solution/preprocess.py
+++ sebi_tree_solution/preprocess.py
@@ -1,6 +1,8 @@
 import subprocess
 from pathlib import Path
 import os
+import requests
+import time
 
 proc = subprocess.Popen(
     ["python", "fastapi_app.py"],
@@ -9,3 +11,10 @@ proc = subprocess.Popen(
     stdout=open(os.devnull, 'w'),
     stderr=open(os.devnull, 'w')
 )
+
+for trial in range(10):
+    try:
+        requests.get('http://127.0.0.1:8000/szarosapi')
+        time.sleep(5)
+    except:
+        print(f"Server is not running at trial {trial}")
