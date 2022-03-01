10ffb6ec67e4f9c051dca040c27fee84c64e6b4c
arturvelk <64686836+arturvelk@users.noreply.github.com>
2022-03-01 02:43:44 +0100

    nézzük mit tud, hogyha 4-be vágom, vajon ez így elég?

--- nyitottagy/process.py
+++ nyitottagy/process.py
@@ -7,16 +7,26 @@ import json
 
 if __name__ == "__main__":
 
+    def compacting(df, input_array):
+        index = []
+        for i in input_array:
+            d = scipy.spatial.distance.cdist(list(df.values), i).astype(np.float32)
+            index += ([d.T[i].argmin() for i in range(len(d.T))])
+        return index
+
     data = pd.read_pickle("data.pkl")
     df = data.reset_index().loc[:,["x_position","y_position","z_position"]]
 
     input_locations = json.loads(Path("input.json").read_text())
-
+    #cut input into 4
     input = np.array([list(i.values()) for i in input_locations])
+    if len(input_locations) == 5000:
+        input = np.array_split(input, 4)
+        indexes = compacting(df, input)
+    else:
+        d = scipy.spatial.distance.cdist(list(df.values), input).astype(np.float32)
 
-    d = scipy.spatial.distance.cdist(list(df.values), np.array(input))
+        indexes = [d.T[i].argmin() for i in range(len(d.T))]
 
-    shortest_index = [d.T[i].argmin() for i in range(len(d.T))]
-
-    result = data.iloc[shortest_index, [0, -2, -1]].to_dict("records")
+    result = data.iloc[indexes, [0, -2, -1]].to_dict("records")
     Path("output.json").write_text(json.dumps(result))
