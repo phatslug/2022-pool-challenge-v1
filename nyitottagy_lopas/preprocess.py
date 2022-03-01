23ec85b6f0bc3387ec8bcef25821fa06cfae8189
arturvelk <64686836+arturvelk@users.noreply.github.com>
2022-03-01 18:08:43 +0100

    asdasd

--- nyitottagy_lopas/preprocess.py
+++ nyitottagy_lopas/preprocess.py
@@ -1,26 +1,26 @@
 import pandas as pd
 from sklearn.neighbors import KDTree
 import pickle
+import numpy as np
 
 data = pd.read_csv("data.csv")
 
-data = data.loc[data["entity_id"] == 0].drop_duplicates().reset_index()
+cols_1 = ['msec', 'x_position', 'y_position', 'z_position', 'subject', 'trial']
+cols = ["x_position","y_position","z_position"]
+
+data = data.loc[data["entity_id"] == 0].drop_duplicates(["msec","trial","subject"]).dropna().reset_index(drop = True)
+data = data.loc[:,cols_1]
+
+for i in ["x_position", "y_position","z_position"]:
+    data[i] = data[i].astype(np.float32)
+
+data["msec"] = data["msec"].astype(np.int32)
+data["trial"] = data["trial"].astype(np.int8)
+data["subject"] = data["subject"].astype("category")
+
 data.to_pickle("data.pkl")
 
-pos_cols = [f"{ax}_position" for ax in ["x", "y", "z"]]
-
-data_subset = (
-    pd.read_csv("data.csv")[lambda _df: _df["keycode"] == "p"]
-    .drop_duplicates(subset=["msec", "subject", "trial"])
-    .dropna()
-    .reset_index()
-)
-
-pos_cols = [f"{ax}_position" for ax in ["x", "y", "z"]]
-
-tree = KDTree(data_subset.loc[:, pos_cols].to_numpy(), leaf_size=1)
-
-data_subset.to_parquet("data_subset.parquet")
+tree = KDTree(data.loc[:, ["msec", "trial","subject"]].to_numpy(), leaf_size=1)
 
 with open("kdtree.pickle", "wb") as file:
-    pickle.dump(tree, file)
+        pickle.dump(tree, file)
