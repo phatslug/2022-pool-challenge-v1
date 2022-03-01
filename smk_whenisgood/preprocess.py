f9fba0db96474a37ddcfecd3438f8d2812414e31
Kofrán Dániel <63071083+phatslug@users.noreply.github.com>
2022-03-01 09:25:00 +0100

    Add files via upload

--- smk_whenisgood/preprocess.py
+++ smk_whenisgood/preprocess.py
@@ -11,7 +11,7 @@ data_subset = (
 
 pos_cols = [f"{ax}_position" for ax in ["x", "y", "z"]]
 
-tree = KDTree(data_subset.loc[:, pos_cols].to_numpy(), leaf_size=1, metric = "manhattan")
+tree = KDTree(data_subset.loc[:, pos_cols].to_numpy(), leaf_size=1, metric = "euclidean")
 
 data_subset.loc[:,["msec", "subject", "trial"]].to_parquet("data_subset.parquet")
 
