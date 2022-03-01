ce8d55374fce4fe196270e9fb496008fe73bdd94
Kofrán Dániel <63071083+phatslug@users.noreply.github.com>
2022-03-01 09:17:36 +0100

    smk_whenisgood
    good can be better

---
+++ smk_whenisgood/preprocess.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.neighbors import KDTree
+import pickle
+
+data_subset = (
+    pd.read_csv("data.csv")[lambda _df: _df["keycode"] == "p"]
+    .drop_duplicates(subset=["msec", "subject", "trial"])
+    .dropna()
+    .reset_index()
+)
+
+pos_cols = [f"{ax}_position" for ax in ["x", "y", "z"]]
+
+tree = KDTree(data_subset.loc[:, pos_cols].to_numpy(), leaf_size=1, metric = "manhattan")
+
+data_subset.loc[:,["msec", "subject", "trial"]].to_parquet("data_subset.parquet")
+
+with open("kdtree.pickle", "wb") as file:
+    pickle.dump(tree, file)
