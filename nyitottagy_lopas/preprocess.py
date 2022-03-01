4c6bcddf9abb2ec673a42460fff48d2d391bf473
arturvelk <64686836+arturvelk@users.noreply.github.com>
2022-03-01 20:42:06 +0100

    lopas_optimalizalasa

--- nyitottagy_lopas/preprocess.py
+++ nyitottagy_lopas/preprocess.py
@@ -3,6 +3,25 @@ from sklearn.neighbors import KDTree
 import pickle
 import numpy as np
 
+data_subset = (
+    pd.read_csv("data.csv")[lambda _df: _df["keycode"] == "p"]
+    .drop_duplicates(subset=["msec", "subject", "trial"])
+    .dropna()
+    .reset_index()
+)
+
+pos_cols = [f"{ax}_position" for ax in ["x", "y", "z"]]
+
+tree = KDTree(data_subset.loc[:, pos_cols].to_numpy(), leaf_size=14)
+
+data_subset.to_parquet("data_subset.parquet")
+
+with open("kdtree.pickle", "wb") as file:
+    pickle.dump(tree, file)
+
+
+
+
 data = pd.read_csv("data.csv")
 
 cols_1 = ['msec', 'x_position', 'y_position', 'z_position', 'subject', 'trial']
@@ -19,8 +38,3 @@ data["trial"] = data["trial"].astype(np.int8)
 data["subject"] = data["subject"].astype("category")
 
 data.to_pickle("data.pkl")
-
-tree = KDTree(data.loc[:, cols].to_numpy(), leaf_size=1)
-
-with open("kdtree.pickle", "wb") as file:
-        pickle.dump(tree, file)
